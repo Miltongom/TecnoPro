065e887afc4119ba7ec387aa1d3a9edf8901554a
Milton Joetd Gómez Marroquín <joetd2003@gmail.com>
2025-02-12 11:23:27 -0600

    update: update MVC base structure for the landing page

---
+++ web/views/navbar.php
@@ -0,0 +1,8 @@
+<aside class="navbar">
+    <ul>
+        <li><a href="index.php?page=laptops">Laptops</a></li>
+        <li><a href="index.php?page=escritorio">Escritorio</a></li>
+        <li><a href="index.php?page=mac">MAC</a></li>
+        <li><a href="index.php?page=windows">Windows</a></li>
+    </ul>
+</aside>
